f19bf97b74bcf28feebf54e3e5022d476c624bb9
MaheshwarAnup <72379817+MaheshwarAnup@users.noreply.github.com>
2021-02-13 08:50:22 +0530

    Update config.py

--- quiz/config.py
+++ quiz/config.py
@@ -1,6 +1,6 @@
 class Config:
     """Bot setup config implementation."""
 
-    token: str = ""  # should be API token provided by @botfather
+    token: str = "1617504158:AAH-VZkav_fCxN4hHoIz82GehNL0_SdXcsY"
     db_name: str = "melody.db"
     shelve_name: str = "shelve"
